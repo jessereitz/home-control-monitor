928b8247fd93a60634695160d9fcb4153703bd4e
Jesse Reitz <jessereitz1@gmail.com>
2018-11-11 23:30:16 -0600

    Fix all bugs with shutdown endpoint. Add reboot endpoint.

--- index.js
+++ index.js
@@ -9,24 +9,60 @@ app.use(bodyParser.json());
 
 const key = 1234;
 
-app.post('/', (req, res) => {
-  if (!req.body) res.send({
+function authenticationError(res) {
+  res.send({
     status: 'error',
     msg: 'Authentication error.'
   });
-  if (req.key === key) {
-    exec('shutdown +1', (err) => {
-      const returnObj = {};
-      if (err) {
-        returnObj.status = 'error';
-        returnObj.msg = 'Schedule shutdown unsuccessful.';
-      } else {
-        returnObj.status = 'success';
-        returnObj.msg = 'Shutdown schedules one minute from now.';
-      }
-      res.send(returnObj);
-    });
-  }
+  return null;
+}
+
+/**
+ * shutdown - Shuts down server. Proper key must be provided in post body.
+ *
+ * @returns {JSON} Returns an object with the status of the operation in the form:
+ *  {
+ *     status: error, success, or info
+ *     msg: A message describing the current state.
+ *  }
+ */
+app.post('/shutdown', (req, res) => {
+  if (!req.body || req.body.key !== key) return authenticationError(res);
+  exec('shutdown +1', (err) => {
+    const returnObj = {};
+    if (err) {
+      returnObj.status = 'error';
+      returnObj.msg = 'Schedule shutdown unsuccessful.';
+    } else {
+      returnObj.status = 'info';
+      returnObj.msg = 'Shutdown scheduled one minute from now.';
+    }
+    res.send(returnObj);
+  });
 });
+
+/**
+ * restart - Restarts server. Proper key must be provided in post body.
+ *
+ * @returns {JSON} Returns an object with the status of the operation in the form:
+ *  {
+ *     status: error, success, or info
+ *     msg: A message describing the current state.
+ *  }
+ */
+app.post('/restart', (req, res) => {
+  if (!req.body || req.body.key !== key) return authenticationError(res);
+  exec('shutdown -r +1', (err) => {
+    const returnObj = {};
+    if (err) {
+      returnObj.status = 'error';
+      returnObj.msg = 'Schedule reboot unsuccessful.';
+    } else {
+      returnObj.status = 'info';
+      returnObj.msg = 'Reboot scheduled one minute from now.';
+    }
+    res.send(returnObj);
+  })
+})
 
 app.listen(port);
